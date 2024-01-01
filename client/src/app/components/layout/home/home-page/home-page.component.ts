b56586c3c42fe2db6c8d9938a612e9b8795e47d3
Ansel Chang <ansel.chang@yahoo.com>
2023-12-31 21:03:22 -0500

    still not working but maybe progress

--- client/src/app/components/layout/home/home-page/home-page.component.ts
+++ client/src/app/components/layout/home/home-page/home-page.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { getTopMoves } from 'client/src/app/libraries/wasm-stackrabbit/wasm-stackrabbit';
+import { testWASM } from 'client/src/app/libraries/wasm-stackrabbit/wasm-stackrabbit-interface';
 
 @Component({
   selector: 'app-home-page',
@@ -14,9 +14,7 @@ export class HomePageComponent implements OnInit {
 
   async ngOnInit() {
 
-    const req = "00000000000000000000000000000000000000000000000000000000000000011000000001100100000110110000011111000011111100011111110001111111001111111100111111111011111111101111111110111111111011111111101111111110|19|192|2|0|X.|200|6|";
-    const res = getTopMoves(req);
-    console.log(res);
+    testWASM();
 
   }
 
