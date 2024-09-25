ac83e76b8b3a68d44411b7611ffde46013db456d
Ansel Chang <ansel.chang@yahoo.com>
2024-09-25 19:23:45 -0400

    sql

---
+++ reset-puzzle-set.sql
@@ -0,0 +1,8 @@
+-- Clear puzzle_feedback table
+DELETE FROM public.puzzle_feedback;
+
+-- Clear puzzle_attempts table
+DELETE FROM public.puzzle_attempts;
+
+-- Clear rated_puzzles table
+DELETE FROM public.rated_puzzles;
