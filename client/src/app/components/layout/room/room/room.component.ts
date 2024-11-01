e0858bf0f281394aa4b2d8059feeadc10c8a255e
Ansel Chang <ansel.chang@yahoo.com>
2024-11-01 12:46:15 -0400

    solo room nes layout

--- client/src/app/components/layout/room/room/room.component.ts
+++ client/src/app/components/layout/room/room/room.component.ts
@@ -2,16 +2,18 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RoomService } from 'src/app/services/room/room.service';
 import { map } from 'rxjs';
 import { RoomType } from 'src/app/shared/room/room-models';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-room',
   templateUrl: './room.component.html',
   styleUrls: ['./room.component.scss']
 })
-export class RoomComponent implements OnDestroy {
+export class RoomComponent implements OnInit, OnDestroy {
 
   constructor(
     public readonly roomService: RoomService,
+    private readonly router: Router
   ) {}
 
   // The type of the room
@@ -20,9 +22,17 @@ export class RoomComponent implements OnDestroy {
   );
   readonly RoomType = RoomType;
 
+  async ngOnInit() {
+    // If not in room, redirect to home
+    if (this.roomService.getRoomState() === null) {
+      this.router.navigate(['/']);
+    }
+  }
+
   public async sendMessage(message: string) {
     await this.roomService.sendChatMessage(message);
   }
+
 
   // Leave the room when the component is destroyed
   async ngOnDestroy() {
